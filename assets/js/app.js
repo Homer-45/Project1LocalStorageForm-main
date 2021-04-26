ee9c351de092668e10f0a9c8a49fe86079183aa5
Homer <hhonasan@sorsogonstatecollege.edu.ph>
2021-04-26 14:05:14 +0800

    Removing elements from the DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 //Variables
-
+const tweetList = document.getElementById('tweet-list');
 
 
 //Event Listeners
@@ -16,5 +16,22 @@ function eventListeners() {
 function newTweet(e){
     e.preventDefault();
 
-    console.log('Form Submitted');
+   //Read the textarea value
+	const tweet = document.getElementById('tweet').value;
+
+   //Create the remove button
+    const removeBtn = document.createElement('a');
+	removeBtn.classList = 'remove-tweet';
+	removeBtn.textContent = 'X';
+
+    //Create an <li> element
+	const li = document.createElement('li');
+	li.textContent = tweet;
+    tweetList.appendChild(li);
+
+    //Add the remove button of each tweet
+	li.appendChild(removeBtn);
+
+    //Add to the list
+	tweetList.appendChild(li);
 }
